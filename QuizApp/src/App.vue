3ac59ed8dd01a8675d3d25b0ba0b3dcbb2446fab
hatice <haticekatranci@yandex.com>
2023-03-28 16:36:56 +0300

    add components

--- QuizApp/src/App.vue
+++ QuizApp/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import q from "./data/quizes.json"
 import {ref, watch} from "vue"
+import Card from "./components/Card.vue"
 
 const quizes = ref (q)
 const search = ref("")
@@ -19,13 +20,8 @@ watch(search, () =>{
           <input v-model.trim="search" type="text" placeholder="search...">
       </header>
       <div class="options-container">
-          <div v-for="quiz in quizes" :key="quiz.id" class="card">
-              <img :src="quiz.img" alt="math">
-              <div class="card-text">
-                  <h2>{{ quiz.name }}</h2>
-                  <p>{{ quiz.questions.length }}</p>
-              </div>
-          </div>
+          <Card v-for="quiz in quizes" :key="quiz.id" :quizImg="quiz.img" :quiz="quiz"  />
+
       </div>
   </div>
 </template>
@@ -58,32 +54,4 @@ header input {
     margin-top: 40px;
 }
 
-/**card**/
-.card {
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px, 1px, 10px rgb(0,0,0,0.1);
-    margin-bottom: 35px;
-    margin-right: 20px;
-    cursor: pointer;
-}
-.card img {
-    width: 100%;
-    height: 190px;
-    margin: 0;
-
-}
-.card .card-text {
-    padding: 0 5px;
-}
-
-.card .card-text h2 {
-    font-weight: bold;
-
-}
-
-
 </style>
-<script setup>
-</script>
